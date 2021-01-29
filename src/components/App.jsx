9f017708724da84cef41f9d095b5bc143d845966
lmwang27 <52217891+lmwang27@users.noreply.github.com>
2021-01-28 18:52:28 -0800

    adding one handlechange function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fullName, setFName] = useState({
+    fName: "",
+    lName: ""
+  });
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    const name = event.target.name;
+    if (name === "fName") {
+      setFName({ fName: newValue, lName: fullName.lName });
+    } else {
+      setFName({ fName: fullName.fName, lName: newValue });
+    }
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {fullName.fName} {fullName.lName}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input
+          name="fName"
+          onChange={handleChange}
+          placeholder="First Name"
+          value={fullName.fName}
+        />
+        <input
+          name="lName"
+          onChange={handleChange}
+          placeholder="Last Name"
+          value={fullName.lName}
+        />
         <button>Submit</button>
       </form>
     </div>
